a0d8f6f620136f79c3e28eba67d3d95c1e05ca2c
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2026-06-04 21:29:08 -0300

    feat: criado tool para validar token do mcp

---
+++ mcp/index.ts
@@ -0,0 +1,14 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import authenticate from "./tools/authentication.ts";
+
+const server = new McpServer({
+  name: "token-authentication",
+  version: "0.1.0",
+  description: "MCP server for token authentication",
+})
+
+await authenticate(server);
+
+const transport = new StdioServerTransport();
+server.connect(transport);
